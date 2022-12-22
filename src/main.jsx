90cfc52cbe57a2af24d607ee6c24ada8898a063d
bryancol27 <bryan.2709cas@gmail.com>
2022-12-22 12:28:41 -0500

    Eslint commit

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <div>
-      hola
-    </div>
-  </React.StrictMode>,
-)
+    <React.StrictMode>
+        <div>
+          hola
+        </div>
+    </React.StrictMode>,
+);
